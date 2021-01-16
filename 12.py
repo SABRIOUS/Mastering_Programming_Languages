6ff3cfd9457b445ab66e543f40f3ec42df63a954
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-17 00:14:03 +0200

    max num without for or while loop

---
+++ 12.py
@@ -0,0 +1,26 @@
+# maximum_num
+num1,num2,num3,num4,num5,num6,num7,num8,num9,num10= map(int,input("enter 10 numbers: ").split())
+
+max_num = 0
+
+if num1 > max_num:
+    max_num = num1
+if num2 > max_num:
+    max_num = num2
+if num3 > max_num:
+    max_num = num3
+if num4 > max_num:
+    max_num = num4
+if num5 > max_num:
+    max_num = num5
+if num6 > max_num:
+    max_num = num6
+if num7 > max_num:
+    max_num = num7
+if num8 > max_num:
+    max_num = num8
+if num9 > max_num:
+    max_num = num9
+if num10 > max_num:
+    max_num = num10
+print(max_num)
