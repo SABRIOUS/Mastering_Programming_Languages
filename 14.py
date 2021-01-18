d7eb79a9c6e852f61e6f00ff47f0fc1ddf43639c
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-18 14:55:00 +0200

    number of digits form an integer

--- 14.py
+++ 14.py
@@ -1,10 +1,12 @@
 # given an integer then count the number of digits
 x = int(input("Enter x "))
+num = x
+if x == 0:
+    count_integer = 1
+else:
+    count_integer = 0
+    while x > 0:
+        x = x //10
+        count_integer += 1
 
-count_integer = 0
-
-while x > 0:
-    x = x //10
-    count_integer += 1
-
-print(count_integer)
+print("the number of digits of",num,"is",count_integer)
