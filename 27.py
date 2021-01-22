7afcc4eadcc9537028f5ed2f116e1e1ba8ea1061
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-22 17:25:20 +0200

    new method

--- 27.py
+++ 27.py
@@ -2,8 +2,15 @@ n = int(input("Enter a num: "))
 m = int(input("Enter a second num: "))
 sum = int(input("Enter a third num: "))
 count = 0
-for a in range(1,n+1):
-    for b in range(1,m+1):
-        if a + b == sum:
-            count += 1
-print(count)            
+# for a in range(1,n+1):
+#     for b in range(1,m+1):
+#         if a + b == sum:
+#             count += 1
+for i in range(1,n+1):
+    # we already know the sum num and i
+    # and we need to know j from simple mathematics equations
+    # sum = i + j if we make a new for loop
+    j = sum -i
+    if j >= 1 and j <= m:
+        count += 1
+print(count)
