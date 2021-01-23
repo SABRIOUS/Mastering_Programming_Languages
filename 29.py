0e4c6f0ec1b38ade76bcf0b558a0c623cb1e6b5b
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-23 17:41:51 +0200

    prime num

--- 29.py
+++ 29.py
@@ -1,3 +1,6 @@
-for i in range(20):
-    if i % 2 != 0 and i % 3 != 0:
-        print(i)
+n = int(input("Enter a num: "))
+for i in range(n):
+    if i == 2 or i == 3:
+        print(i,end = " ")
+    if i > 1 and i % 2 != 0 and i % 3 != 0:
+        print(i,end = " ")
