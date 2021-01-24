ca81d8349a05bc8160d791bf9ee47dbe38db3fad
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-25 00:25:15 +0200

    while

--- 21.py
+++ 21.py
@@ -2,7 +2,7 @@
 n = int(input("Enter how many Try: "))
 while n > 0:
     x = int(input("how many number you want to get: "))
-    min_num = 20
+    min_num = 0
     while x > 0:
         y= int(input("Enter a num: "))
         if y < min_num:
