6cd1517a5e7514f257544e66c0e8e66183207e28
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-27 16:00:49 +0200

    reversed python

---
+++ 35.py
@@ -0,0 +1,13 @@
+# reverse with python
+lst = [100,220,30,1,5,900]
+
+# for i in range(len(lst)): #c++ style
+#     print(lst[len(lst)- i-1])
+
+# for i in range(len(lst)-1,-1,-1):
+#     print(lst[i],end = ' ')
+
+# for item in reversed(lst):
+#     print(item,end= ' ')
+# for idx,item in reversed(list(enumerate(lst))):
+#     print(idx,item,end = ' - ')
