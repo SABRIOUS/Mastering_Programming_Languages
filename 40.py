06c19eb71bfe724b9c36032a17bd1c6fb5a0a619
SABRIOUS <ahmedsabrious@gmail.com>
2021-01-31 21:55:20 +0200

    list_comprehensions_target

--- 40.py
+++ 40.py
@@ -1,6 +1,9 @@
 def find_smallest(lst,target_type):
-    return min([i for i in lst if type(i)==target_type])
 
+    new_lst = [i for i in lst if type(i)==target_type]
+    if len(new_lst) == 0:
+        return None
+    return min(new_lst)
 if __name__ == '__main__':
     lst = [10,-2.5,20,5,'ahmed',5.2,'mohamed']
     print(find_smallest(lst,type(0)))
