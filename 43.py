88ee15c261ba33ff0287de92adadd0d340a4267d
SABRIOUS <ahmedsabrious@gmail.com>
2021-02-12 14:50:02 +0200

    nearest_value

---
+++ 43.py
@@ -0,0 +1,29 @@
+def nearest_value(set_n: set, target: int) -> int:
+    get_min = {}
+    result = ""
+    for num in set_n:
+        get_min[num] = abs(target-num)
+    s_lst = list(get_min.keys())
+    for i in s_lst:
+        if target - i == 1:
+            result = i
+
+        else:
+            result = min(get_min,key=get_min.get)
+    return result
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
