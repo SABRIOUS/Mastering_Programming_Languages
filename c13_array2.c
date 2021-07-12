bbd04d09288ecb9edb6588f713b0574f266c2423
SABRIOUS <ahmedsabrious@gmail.com>
2021-07-12 13:28:00 +0200

    array_2

---
+++ c13_array2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// برنامج يطلب من الشخص يدخله 10 ارقام و يعد كام رقم من اللى دخلهم = 5
+// و لو مش موجود يطبع " not found " و بعد البرنامج ما يخلص يتعاد تانى لوحده
+
+// int main(void){
+//   int k = 1;
+//   while (k){
+//     int i,n,x,count_five=0;
+//     printf("How Many number you want to add to array: ");
+//     scanf("%d",&n);
+//     int arr[n];
+//
+//     for (i = 0; i < n; i++){
+//       printf("add number %d to array: ",i);
+//       scanf("%d",&x);
+//       arr[i] = x;
+//     }
+//
+//     for (i = 0; i < n; i++){
+//       if (arr[i] == 5)
+//       count_five += 1;
+//     }
+//
+//     if (count_five == 0)
+//       printf("\nNot found\n");
+//     else {
+//       printf("\nFound\n");
+//     }
+//     printf("\n Do you want restart Enter 0 to stop the programe or 1 to continue: ");
+//     scanf("%d",&k);
+//   }
+//   return 0;
+//
+// }
+
+// ----------------------
