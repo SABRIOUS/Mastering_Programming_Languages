ffa28aa9f62483f43fe79051cdc3b917fa630f29
SABRIOUS <ahmedsabrious@gmail.com>
2021-07-21 22:23:29 +0200

    c language struct

---
+++ c22_struct.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+// struct student{
+//     char name[20],phone[20];
+//     int videos;
+// };
+
+// int main(){
+//   struct student x;
+//   printf("Enter your name: ");
+//   scanf("%s", x.name);
+//   printf("Enter your Phone: ");
+//   scanf("%s", x.phone);
+//   x.videos = 0;
+//
+//   printf("Hi %s\n",x.name);
+//   printf("Your Phone is %s and you watched [%d] videos\n",x.phone,x.videos);
+//
+//
+//   return 0;
+// }
+
+// برنامج يخزن اسم الطالب و درجاته فى ا
+// .ل ( math, science ,English )
+
+
+
+// struct student{
+//   char name[20];
+//   int english,science,math;
+// };
+
+
+// int main(){
+//   struct student x;
+//   printf("Student name is =: ");
+//   scanf("%s",x.name);
+//
+//   printf("degree in English is =: ");
+//   scanf("%d",&x.english);
+//
+//   printf("degree in Math is =: ");
+//   scanf("%d",&x.math);
+//
+//   printf("degree in Science is =: ");
+//   scanf("%d",&x.science);
+//
+//
+//   printf("The Student [%s] and his degree is\n"
+//   "[math = (%d) and english  =(%d)]",x.name,x.math,x.science);
+//   return 0;
+// }
+
+
+
+// struct supply{
+//   float hight,weight,size;
+// };
+//
+// int main(){
+//   struct supply x;
+//   printf("Enter height and weight and size: ");
+//   scanf("%f %*c %f %*c %f",&x.hight,&x.weight,&x.size);
+//
+//   printf("the height and %2.2f %2.2f %2.2f\n",x.hight,x.weight,x.size);
+//
+//   return 0;
+// }
