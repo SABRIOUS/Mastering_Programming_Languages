df0d2f32cfec9d57498ecd0c27df28ffa903ccb2
SABRIOUS <ahmedsabrious@gmail.com>
2021-07-24 19:27:39 +0200

    pointers

---
+++ c24_pointers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// make a function to change values
+
+// void change(int *x,int *y){
+//   int t = *x;
+//   // change x  to y
+//   *x = *y;
+//   // change y to be the temp for x
+//   *y = t;
+//
+// }
+//
+// int main(){
+//   int x = 5, y = 20;
+//   change(&x,&y);
+//   printf("x now is %d and y  now is %d\n",x,y);
+//   return 0;
+// }
+
+// -------------------
+
+// void get_sum(int x, int y){
+//   printf("----------------\n");
+//   printf("\nSum of x and y is = %d\n",x+y);
+// }
+//
+// int main(){
+//   int x,y;
+//   printf("Enter the first Num: ");
+//   scanf("%d",&x);
+//   printf("\n");
+//   printf("Enter the second Num: ");
+//   scanf("%d",&y);
+//   get_sum(x,y);
+//
+// }
