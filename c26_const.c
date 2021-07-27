740d5469ef3a1324b7b3eedea977a9fe68d84d60
SABRIOUS <ahmedsabrious@gmail.com>
2021-07-27 12:33:47 +0200

    const in c language

---
+++ c26_const.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+// const
+// int main(){
+//   const int x = 5;
+//   printf("%f\n",x);
+//   return 0;
+// }
+
+// --------------------
+// enum
+// برنامج يرتب ايام الاسبوع و يطبعهم كأرقام
+
+
+// int main(){
+//   enum {sat=1,sun,mon,tue,wen,thu,fri};
+//   printf("%d\n",mon);
+// }
+
+
+// ------------------
+// math.h
+
+int main(){
+  // int x = sqrt(16);
+  // float x = sin(1);
+  printf("%f\n",sqrt(16));
+}
