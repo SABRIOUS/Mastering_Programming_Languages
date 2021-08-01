4e95dcb09998572e7b3bb0f23052fc4ecbfb7a6a
SABRIOUS <ahmedsabrious@gmail.com>
2021-08-01 11:57:45 +0200

    ternary in c language

---
+++ c29_ternary.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+
+// int main(){
+  // int k = 1;
+  // while(k){
+  //   int x;
+  //   printf("\nEnter a number: ");
+  //   scanf("%d",&x);
+  //   if (x%2)
+  //     printf("\nOdd\n");
+  //   else
+  //     printf("\nEven\n");
+  //
+  //   printf("\n\nDo you want to stop 1 for yes  0 for stop: ");
+  //   scanf("%d",&k);
+  // }
+
+// }
+
+
+
+// int main(){
+//   int x;
+//   printf("Enter a number: ");
+//   scanf("%d",&x);
+//
+//   // x%2 if we got 1 or true that's mean reaminder 1 is odd else 0 even
+//     printf(x%2 ? "Odd\n" : "Even\n");
+// }
+
+
+// -----------------------
+// برنامج يطلب من الشخص يدخله رقمين لو الأول أكبر من الثاني، يطبع
+// " first is the greatest " ولو العكس يطبع
+// " second is the greatest "
+// ( باستخدام ال Ternary Operator )
+// Q1
+// int main(){
+//   int first_num,second_num;
+//   printf("Enter the first num: ");
+//   scanf("%d",&first_num);
+//   printf("Enter the second num: ");
+//   scanf("%d",&second_num);
+//
+//   printf(first_num > second_num ? "First is the greatest" : "Second is the greatest");
+// }
+// Q2
+// -----------------------
+ // برنامج يطلب من الشخص يدخله رقمين يجمعهم على بعض و لو المجموع أكبر من
+ // 10 يطبع "more than 10" لو العكس يطبع
+ // "less than 10" ( باستخدام ال Ternary Operator )
+
+// int main(){
+//   int first_num,second_num;
+//   printf("Enter the first num: ");
+//   scanf("%d",&first_num);
+//   printf("Enter the second num: ");
+//   scanf("%d",&second_num);
+//
+//   printf(first_num + second_num > 10 ? "More than 10" : "Less than 10");
+// }
+
+// -----------------------
+// Q3
+// برنامج يطلب من الشخص يدخله رقمين و يطبع الأكبر فيهم ( باستخدام ال
+//   Ternary Operator
+
+
+// int main(){
+//   int first_num,second_num;
+//   printf("Enter the first num: ");
+//   scanf("%d",&first_num);
+//   printf("Enter the second num: ");
+//   scanf("%d",&second_num);
+//
+//   first_num > second_num ? printf( "%d",first_num) : printf("%d", second_num);
+// }
+
+
+// -------------------------
+
+// Q4
+// برنامج يطلب من الشخص يدخله 3 أرقام و يطبع الأكبر فيهم ( باستخدام ال
+//   Ternary Operator
+
+int main(){
+  printf("www.")?printf("gammal."):printf("tech");
+
+}
