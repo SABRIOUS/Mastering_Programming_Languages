c0abce985c232f9762ca4feba5591da2a7bc4f71
SABRIOUS <ahmedsabrious@gmail.com>
2021-08-08 17:33:10 +0200

    append to file in c language

---
+++ c37_storeData.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// int main(){
+//   FILE *con = fopen("con.txt","a+");
+//   // a+ mean add and read at the same time
+//   //  لو الملف مش موجود مش هيحصل مشكلة
+//   char ch,name[100],number[100];
+//   while(fscanf(con,"%c",&ch) != EOF)
+//     printf("%c",ch);
+//
+//
+//   printf("Enter name: ");
+//   scanf("%s",name);
+//   printf("Enter number: ");
+//   scanf("%s",number);
+//
+//   fprintf(con,"%s %s\n",name,number);
+//   return 0;
+// }
+
+
+// برنامج يعمل file جديد و يطلب من المستخدم يدخله 5
+//  اسماء و ارقام تليفوناتهم و يسجل البيانات جوة ال
+//   file و فى النهاية يقوم بطباعة ال file على الشاشة
+
+// int main(){
+//   FILE *namePhone = fopen("list.txt","a+");
+//   // c represent each character
+//   char c,name[100],number[100];
+//   int n = 5;
+//   while(fscanf(namePhone,"%c",&c) != EOF)
+//       printf("%c",c);
+//
+//   while (n>0){
+//     printf("Enter name: ");
+//     scanf("%s",name);
+//     printf("Enter number: ");
+//     scanf("%s",number);
+//     fprintf(namePhone,"%s %s\n",name,number);
+//     printf("\n");
+//     n--;
+//   }
+//
+// }
+
+// ------------------------------
+int main(){
+  printf("\n");
+}
