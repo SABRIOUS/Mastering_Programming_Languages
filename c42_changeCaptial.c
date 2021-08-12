d7753ea4ca1552820d8340052c1fccb52fc5a208
SABRIOUS <ahmedsabrious@gmail.com>
2021-08-12 15:20:48 +0200

    Transform all words to be small

---
+++ c42_changeCaptial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// برنامج فيه file فيه كلام كتير و المطلوب أغير كل الحروف ال
+// capital اللى فى الكلام دة لحروف small
+
+// int main(){
+//   FILE *cap = fopen("captial.txt","r");
+//   FILE *sm = fopen("small.txt","w");
+//   char c;
+//
+//   while(fscanf(cap,"%c",&c)!= EOF)
+//     if(c>=65 & c<= 90)
+//       fprintf(sm,"%c",c+32);
+//     else
+//       fprintf(sm,"%c",c);
+//
+//     fclose(cap);
+//     fclose(sm);
+//
+//   return 0;
+// }
+
+// -------------------------
+int main(){
+  printf("MY NAME\n");
+  return 0;
+}
