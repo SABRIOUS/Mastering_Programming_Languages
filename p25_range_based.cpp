0c882aeb70df041a3c2906bc9007734574e8c43f
SABRIOUS <ahmedsabrious@gmail.com>
2021-09-04 21:27:57 +0200

    range based

---
+++ p25_range_based.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+int main(){
+  // vector<int> x = {5,9,10,3,2,78};
+  // vector<string> y = {"Ahmed","Sabry","Mohamed"};
+  // map<string,int> m = {{"A",98},{"M",100},{"E",93}};
+  // for(int i : x)
+    // cout << i << " ";
+  //
+  // for(auto i : m) //good when you know that all array is full
+  //   cout << i.first <<" "<<i.second<<endl;
+}
