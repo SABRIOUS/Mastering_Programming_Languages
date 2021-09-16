93cb8630252cce0104f1b805c8183245f2213481
SABRIOUS <ahmedsabrious@gmail.com>
2021-09-16 13:35:14 +0200

    logic idea

---
+++ ud4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include <cmath>
+
+int main(){
+  int A = 5;
+  int B = 4;
+  string TorF[] = {"False", "True"};
+  cout << "A > B "<< TorF[A > B]<<endl;
+  return 0;
+}
